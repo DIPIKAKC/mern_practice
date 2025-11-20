efb7d40a2c016966de4229d14dccd23ee10f67c8
Dipika K.C <dipikak0323@gmail.com>
2025-11-20 23:54:23 +0545

    frontend

---
+++ frontend/src/Comps/Header.jsx
@@ -0,0 +1,24 @@
+
+import { Button } from '@/components/ui/button'
+import { NavLink } from 'react-router'
+import DropdownProfile from './DropdownProfile';
+import { useSelector } from 'react-redux';
+
+export default function Header() {
+  const { user } = useSelector((state) => state.userSlice); //userslice ko matra initial state taneko
+  return (
+    <div className='bg-gray-200 flex px-8 py-2 items-center justify-between'>
+      <h1 className='text-2xl font-bold'>Shopal</h1>
+
+      {user ? <DropdownProfile user={user} /> : <div className="space-x-5">
+        <NavLink to={'/login'}>
+          <Button variant="link" className={'text-[16px]'}>Login</Button>
+        </NavLink>
+        <NavLink to={'/signup'}>
+          <Button>Sign Up</Button>
+        </NavLink>
+
+      </div>}
+    </div>
+  )
+}
